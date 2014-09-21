957a6fd3f50ca2f7e950db3db030f423a5fee91f
Mythri Nagaraju <mnagaraju@lbl.gov>
2014-09-21 11:14:24 -0700

    added static finders

--- uberchallenge/settings.py
+++ uberchallenge/settings.py
@@ -85,9 +85,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_ROOT = '/foodtrucks/static/'
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
     '/var/www/static/',
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
