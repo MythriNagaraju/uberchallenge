d05ef585d8352491270d456ae930555c4e33dc54
Mythri Nagaraju <mnagaraju@lbl.gov>
2014-09-21 12:14:12 -0700

    changed STATIC_ROOT

--- uberchallenge/settings.py
+++ uberchallenge/settings.py
@@ -85,7 +85,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT= os.path.join(BASE_DIR, '..', 'uberchallenge_static_files')
+STATIC_ROOT= '/var/www/static/' 
 
 STATIC_URL = '/static/'
 
