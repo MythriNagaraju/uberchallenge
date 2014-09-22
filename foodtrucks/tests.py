42e00c5b540097c90bfe9c7f96d84d3bd62bdc40
Mythri Nagaraju <mnagaraju@lbl.gov>
2014-09-21 23:16:20 -0700

    test index

--- foodtrucks/tests.py
+++ foodtrucks/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import FoodFacility
+from .views import index
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+class IndexView(TestCase):
+    def test_valid_address_should_return_search_results(self):
+	ff = FoodFacility.objects.all()[0]
+	self.get_request = factory.get('/')
+	#self.get_request.user = User.objects.create_user('testuser', password='secret')
+	response = index(self.request)
+	self.assertEqual(response.status_code, 200) 
